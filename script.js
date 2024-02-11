bbf0a0b022fa3d03a81cc092647951781ca43670
ZeBunto <jose99.silva@catolicasc.edu.br>
2024-02-10 21:48:44 -0300

    WIP - Corrigindo erro de conexão

--- script.js
+++ script.js
@@ -1,22 +1,18 @@
-
-let todos = [
-    {
-        text: 'Aprender HTML, CSS e Javascript',
-        done: true
-    },
-    {
-        text: 'Aprender o básico de Vue JS',
-        done: false
-    }
-]
-
-const todosApp={
-    data(){
-        return{
-            todos: []
+const todosApp = Vue.createApp({
+    data() {
+        return {
+            todos: [
+                {
+                    text: 'Aprender HTML, CSS e Javascript',
+                    done: true
+                },
+                {
+                    text: 'Aprender o básico de Vue JS',
+                    done: false
+                }
+            ]
         };
     }
-};
+});
 
-vue.createApp(todosApp).mount('#app');
-
+todosApp.mount('#app');
